077d46e274bb0c15853c2d96c421c6f8e82135b6
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-25 09:50:56 +0800

    Can now get malware relationships.

--- mbctool.py
+++ mbctool.py
@@ -20,6 +20,12 @@ def get_behaviors_by_id(id):
         Filter('id', '=', id)
     ])
 
+def get_malwares_by_id(id):
+    return g_src.query([
+        Filter('type', '=', 'malware'),
+        Filter('id', '=', id)
+    ])
+
 def get_relationships(src_type, rel_type, target_type):
     relationships = g_src.query([
         Filter('type', '=', 'relationship'),
@@ -37,21 +43,34 @@ def get_behavior_relationships(behavior):
 
     return behavior_relationships
 
+def get_malware_relationships(malware):
+    malware_relationships = []
+    relationships = get_relationships('malware', 'uses', 'attack-pattern')
+    for r in relationships:
+        if r.source_ref == malware.id:
+            malware_relationships.append(r)
+
+    return malware_relationships
+
 def setup_src():
     global g_src
     g_src = FileSystemSource('./mbc-stix2/')
 
 if __name__ == '__main__':
     setup_src()
-
-    # behaviors = get_behaviors_by_external_id('B0007')
-
-    behaviors = get_behaviors_by_id('attack-pattern--55040e64-313d-4656-8e1c-1146ff2f47d7')
     
     # objectives = get_all_objectives()
     # for o in objectives:
     #     print(str(o))
 
-    related = get_behavior_relationships(behaviors[0])
+    # behaviors = get_behaviors_by_external_id('B0007')
+
+    # behaviors = get_behaviors_by_id('attack-pattern--55040e64-313d-4656-8e1c-1146ff2f47d7')    
+    # related = get_behavior_relationships(behaviors[0])
+    # for r in related:
+    #     print(str(r))
+
+    malwares = get_malwares_by_id('malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
+    related = get_malware_relationships(malwares[0])
     for r in related:
         print(str(r))
