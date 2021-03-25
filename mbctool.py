f1c3811887ac6a22d3cdbb1824efec4b57d2cc75
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-25 11:07:17 +0800

    Can now get reverse of relationships.

--- mbctool.py
+++ mbctool.py
@@ -26,26 +26,28 @@ def get_malwares_by_id(id):
         Filter('id', '=', id)
     ])
 
-def get_relationships_by(id, src_type, rel_type, target_type):
+def get_relationships_by(id, src_type, rel_type, target_type, is_reversed=False):
     relationship_lists = []
     relationships = g_src.query([
         Filter('type', '=', 'relationship'),
         Filter('relationship_type', '=', rel_type)
     ])
     for r in relationships:
-        if r.source_ref == id:
+        if not is_reversed and r.source_ref == id:
+            relationship_lists.append(r)
+        elif is_reversed and r.target_ref == id:
             relationship_lists.append(r)
 
     return relationship_lists
 
-def get_behavior_relationships(behavior):
-    return get_relationships_by(behavior.id, 'attack-pattern', 'subtechnique-of', 'attack-pattern')
+def get_behavior_relationships(id):
+    return get_relationships_by(id, 'attack-pattern', 'subtechnique-of', 'attack-pattern')
 
-def get_behaviors_used_by_malware(malware):
-    return get_relationships_by(malware.id, 'malware', 'uses', 'attack-pattern')
+def get_behaviors_used_by_malware(id):
+    return get_relationships_by(id, 'malware', 'uses', 'attack-pattern')
 
-def get_malwares_using_behavior(behavior):
-    return get_relationships_by(behavior.id, 'attack-pattern', 'uses', 'malware')
+def get_malwares_using_behavior(id):
+    return get_relationships_by(id, 'malware', 'uses', 'attack-pattern', True)
 
 def setup_src():
     global g_src
@@ -60,14 +62,22 @@ if __name__ == '__main__':
 
     # behaviors = get_behaviors_by_external_id('B0007')
 
-    # behaviors = get_behaviors_by_id('attack-pattern--55040e64-313d-4656-8e1c-1146ff2f47d7')    
+    behaviors = get_behaviors_by_id('attack-pattern--55040e64-313d-4656-8e1c-1146ff2f47d7')    
     # related = get_behavior_relationships(behaviors[0])
     # for r in related:
     #     print(str(r))
 
-    malwares = get_malwares_by_id('malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
-    related = get_behaviors_used_by_malware(malwares[0])
+    # malwares = get_malwares_by_id('malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
+    # related = get_behaviors_used_by_malware(malwares[0].id)
+    # for r in related:
+    #     print(str(r))
+
+    # print('=======')
+    related = get_malwares_using_behavior('attack-pattern--5a3611aa-4253-4302-b09e-02fe53a1af9d')
     for r in related:
         print(str(r))
 
+        
+
+    
     
