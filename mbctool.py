412a3cab480622d6b556df5f0e503e5ccb5121ce
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-25 09:13:22 +0800

    Added initial access functions.

--- mbctool.py
+++ mbctool.py
@@ -8,6 +8,23 @@ def get_all_objectives():
         Filter('type', '=', 'x-mitre-tactic')
     ])
 
+def get_behaviours_by_external_id(external_id):
+    return g_src.query([
+        Filter('type', '=', 'attack-pattern'),
+        Filter('external_references.external_id', '=', external_id)
+    ])
+
+def get_related(src_type, rel_type, target_type):
+    relationships = g_src.query([
+        Filter('type', '=', 'relationship'),
+        Filter('relationship_type', '=', rel_type)
+    ])
+    return relationships
+
+def get_behavior_relationships(behavior):
+    return get_related(None, 'subtechnique-of', None)
+    
+
 def setup_src():
     global g_src
     g_src = FileSystemSource('./mbc-stix2/')
@@ -15,6 +32,15 @@ def setup_src():
 if __name__ == '__main__':
     setup_src()
 
-    objectives = get_all_objectives()
-    for o in objectives:
-        print(str(o))
+    behaviors = get_behaviours_by_external_id('B0007')
+    for b in behaviors:
+        print(str(b))
+    
+    # objectives = get_all_objectives()
+    # for o in objectives:
+    #     print(str(o))
+
+    # related = get_behavior_relationships(None)
+    # for r in related:
+    #     print(str(r))
+    
