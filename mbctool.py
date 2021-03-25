9195caefd702c63fa2a5cd0dc5b9767f1e28e61d
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-25 09:31:15 +0800

    Can now get behavior relationships.

--- mbctool.py
+++ mbctool.py
@@ -8,22 +8,34 @@ def get_all_objectives():
         Filter('type', '=', 'x-mitre-tactic')
     ])
 
-def get_behaviours_by_external_id(external_id):
+def get_behaviors_by_external_id(external_id):
     return g_src.query([
         Filter('type', '=', 'attack-pattern'),
         Filter('external_references.external_id', '=', external_id)
     ])
 
-def get_related(src_type, rel_type, target_type):
+def get_behaviors_by_id(id):
+    return g_src.query([
+        Filter('type', '=', 'attack-pattern'),
+        Filter('id', '=', id)
+    ])
+
+def get_relationships(src_type, rel_type, target_type):
     relationships = g_src.query([
         Filter('type', '=', 'relationship'),
         Filter('relationship_type', '=', rel_type)
     ])
+
     return relationships
 
 def get_behavior_relationships(behavior):
-    return get_related(None, 'subtechnique-of', None)
-    
+    behavior_relationships = []
+    relationships = get_relationships('attack-pattern', 'subtechnique-of', 'attack-pattern')
+    for r in relationships:
+        if r.source_ref == behavior.id:
+            behavior_relationships.append(r)
+
+    return behavior_relationships
 
 def setup_src():
     global g_src
@@ -32,15 +44,14 @@ def setup_src():
 if __name__ == '__main__':
     setup_src()
 
-    behaviors = get_behaviours_by_external_id('B0007')
-    for b in behaviors:
-        print(str(b))
+    # behaviors = get_behaviors_by_external_id('B0007')
+
+    behaviors = get_behaviors_by_id('attack-pattern--55040e64-313d-4656-8e1c-1146ff2f47d7')
     
     # objectives = get_all_objectives()
     # for o in objectives:
     #     print(str(o))
 
-    # related = get_behavior_relationships(None)
-    # for r in related:
-    #     print(str(r))
-    
+    related = get_behavior_relationships(behaviors[0])
+    for r in related:
+        print(str(r))
