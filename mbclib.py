2a4fa48bcff01635f9ea27b176a3e86ef8757dad
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-25 20:57:12 +0800

    mbctool is now mbclib.

--- mbclib.py
+++ mbclib.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 from stix2 import Filter, FileSystemSource
-from argparse import ArgumentParser
-import sys
 
 def get_all_objectives(src):
     return src.query([
@@ -67,46 +65,49 @@ def get_malwares_using_behavior(src, id):
 def setup_src():
     return FileSystemSource('./mbc-stix2/')
 
-if __name__ == '__main__':
-    parser = ArgumentParser(description='MBC Tool')
-    parser.add_argument('-i',
-                        '--id',
-                        help='The ID to search for.')
-    parser.add_argument('-e',
-                        '--externalid',
-                        help='The external ID to search for.')
+# if __name__ == '__main__':
+#     parser = ArgumentParser(description='MBC Tool')
+#     parser.add_argument('-i',
+#                         '--id',
+#                         help='The ID to search for.')
+#     parser.add_argument('-e',
+#                         '--externalid',
+#                         help='The external ID to search for.')
+#     parser.add_argument('-bb',
+#                         '--externalid',
+#                         help='The external ID to search for.')
     
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    src = setup_src()
+#     src = setup_src()
 
-    if args.id:
-        if 'malware--' in args.id:
-            malware = get_malware_by_id(src, args.id)
-            print(str(malware))
-        elif 'attack-pattern--' in args.id:
-            behavior = get_behavior_by_id(src, args.id)
-            print(str(behavior))
-        else:
-            print('[ERROR] ID ' + args.id + ' is not valid.')
-            raise SystemExit(1)
-    elif args.externalid:
-        behavior = get_behavior_by_external_id(src, args.externalid)
-        if behavior:
-            print(str(behavior))
-        else:
-            print('[ERROR] ExternalID ' + args.externalId + ' is not valid.')
-            raise SystemExit(1)
+#     if args.id:
+#         if 'malware--' in args.id:
+#             malware = get_malware_by_id(src, args.id)
+#             print(str(malware))
+#         elif 'attack-pattern--' in args.id:
+#             behavior = get_behavior_by_id(src, args.id)
+#             print(str(behavior))
+#         else:
+#             print('[ERROR] ID ' + args.id + ' is not valid.')
+#             raise SystemExit(1)
+#     elif args.externalid:
+#         behavior = get_behavior_by_external_id(src, args.externalid)
+#         if behavior:
+#             print(str(behavior))
+#         else:
+#             print('[ERROR] ExternalID ' + args.externalId + ' is not valid.')
+#             raise SystemExit(1)
 
-    sys.exit()
+#     sys.exit()
 
-    # malware = get_malware_by_id(src, 'malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
-    # malwares = get_behaviors_used_by_malware(src ,malware.id)
-    # for m in malwares:
-    #     print(str(m))
+#     # malware = get_malware_by_id(src, 'malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
+#     # malwares = get_behaviors_used_by_malware(src ,malware.id)
+#     # for m in malwares:
+#     #     print(str(m))
 
-    # print('=======')
-    # behavior = get_behavior_by_external_id(src, 'B0031')
-    # behaviors = get_malwares_using_behavior(src, behavior.id)
-    # for b in behaviors:
-    #     print(str(b))
+#     # print('=======')
+#     # behavior = get_behavior_by_external_id(src, 'B0031')
+#     # behaviors = get_malwares_using_behavior(src, behavior.id)
+#     # for b in behaviors:
+#     #     print(str(b))
--- test_mbclib.py
+++ test_mbclib.py
@@ -1,4 +1,4 @@
-from mbctool import *
+from mbclib import *
 import pytest
 import subprocess
 import json
@@ -6,18 +6,6 @@ import json
 bid = 'attack-pattern--295a3b88-2a7e-4bae-9c50-014fce6d5739'
 eid = 'B0007'
 mid = 'malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa'
-
-def test_main():
-    src = setup_src()
-
-    b = subprocess.check_output('./mbctool.py -i ' + bid, shell=True)
-    b = json.loads(b)
-    assert b['type'] == 'attack-pattern' and b['id'] == bid
-
-    b = subprocess.check_output('./mbctool.py -e ' + eid, shell=True)
-    b = json.loads(b)
-    assert b['type'] == 'attack-pattern' and b['id'] == bid
-
 
 def test_lib():
     src = setup_src()
