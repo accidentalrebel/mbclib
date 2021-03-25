90e824d2c399ff387d02d00d75be326b68b48645
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-25 11:36:52 +0800

    More refactoring.

--- mbctool.py
+++ mbctool.py
@@ -14,17 +14,19 @@ def get_behaviors_by_external_id(external_id):
         Filter('external_references.external_id', '=', external_id)
     ])
 
-def get_behaviors_by_id(id):
-    return g_src.query([
+def get_behavior_by_id(id):
+    q = g_src.query([
         Filter('type', '=', 'attack-pattern'),
         Filter('id', '=', id)
     ])
+    return q[0] if len(q) > 0 else None
 
-def get_malwares_by_id(id):
-    return g_src.query([
+def get_malware_by_id(id):
+    q =  g_src.query([
         Filter('type', '=', 'malware'),
         Filter('id', '=', id)
     ])
+    return q[0] if len(q) > 0 else None
 
 def get_relationships_by(id, src_type, rel_type, target_type, is_reversed=False):
     relationship_lists = []
@@ -44,10 +46,22 @@ def get_behavior_relationships(id):
     return get_relationships_by(id, 'attack-pattern', 'subtechnique-of', 'attack-pattern')
 
 def get_behaviors_used_by_malware(id):
-    return get_relationships_by(id, 'malware', 'uses', 'attack-pattern')
+    rels =  get_relationships_by(id, 'malware', 'uses', 'attack-pattern')
+
+    l = []
+    for rel in rels:
+        l.append(get_behavior_by_id(rel.target_ref))
+
+    return l
 
 def get_malwares_using_behavior(id):
-    return get_relationships_by(id, 'malware', 'uses', 'attack-pattern', True)
+    rels = get_relationships_by(id, 'malware', 'uses', 'attack-pattern', True)
+
+    l = []
+    for rel in rels:
+        l.append(get_malware_by_id(rel.source_ref))
+        
+    return l
 
 def setup_src():
     global g_src
@@ -62,20 +76,20 @@ if __name__ == '__main__':
 
     # behaviors = get_behaviors_by_external_id('B0007')
 
-    behaviors = get_behaviors_by_id('attack-pattern--55040e64-313d-4656-8e1c-1146ff2f47d7')    
-    # related = get_behavior_relationships(behaviors[0])
+    behavior = get_behavior_by_id('attack-pattern--55040e64-313d-4656-8e1c-1146ff2f47d7')    
+    # related = get_behavior_relationships(behavior)
     # for r in related:
     #     print(str(r))
 
-    # malwares = get_malwares_by_id('malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
-    # related = get_behaviors_used_by_malware(malwares[0].id)
-    # for r in related:
-    #     print(str(r))
+    malware = get_malware_by_id('malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
+    malwares = get_behaviors_used_by_malware(malware.id)
+    for m in malwares:
+        print(str(m))
 
-    # print('=======')
-    related = get_malwares_using_behavior('attack-pattern--5a3611aa-4253-4302-b09e-02fe53a1af9d')
-    for r in related:
-        print(str(r))
+    print('=======')
+    behaviors = get_malwares_using_behavior('attack-pattern--5a3611aa-4253-4302-b09e-02fe53a1af9d')
+    for b in behaviors:
+        print(str(b))
 
         
 
