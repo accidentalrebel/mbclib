14a4151666315714ab763aa389abe9167b526fa8
AccidentalRebel <accidentalrebel@gmail.com>
2021-03-25 14:13:50 +0800

    Removed dependency on g_src global variable.

--- mbctool.py
+++ mbctool.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python3
 from stix2 import Filter, FileSystemSource
 
-g_src = None
-
-def get_all_objectives():
-    return g_src.query([
+def get_all_objectives(src):
+    return src.query([
         Filter('type', '=', 'x-mitre-tactic')
     ])
 
-def get_behavior_by_external_id(external_id):
-    q = g_src.query([
+def get_behavior_by_external_id(src, external_id):
+    q = src.query([
         Filter('type', '=', 'attack-pattern'),
         Filter('external_references.external_id', '=', external_id)
     ])
     return q[0] if len(q) > 0 else None
 
-def get_behavior_by_id(id):
-    q = g_src.query([
+def get_behavior_by_id(src, id):
+    q = src.query([
         Filter('type', '=', 'attack-pattern'),
         Filter('id', '=', id)
     ])
     return q[0] if len(q) > 0 else None
 
-def get_malware_by_id(id):
-    q =  g_src.query([
+def get_malware_by_id(src, id):
+    q =  src.query([
         Filter('type', '=', 'malware'),
         Filter('id', '=', id)
     ])
     return q[0] if len(q) > 0 else None
 
-def get_relationships_by(id, src_type, rel_type, target_type, is_reversed=False):
+def get_relationships_by(src, id, src_type, rel_type, target_type, is_reversed=False):
     relationship_lists = []
-    relationships = g_src.query([
+    relationships = src.query([
         Filter('type', '=', 'relationship'),
         Filter('relationship_type', '=', rel_type)
     ])
@@ -43,41 +41,40 @@ def get_relationships_by(id, src_type, rel_type, target_type, is_reversed=False)
 
     return relationship_lists
 
-def get_behavior_relationships(id):
-    return get_relationships_by(id, 'attack-pattern', 'subtechnique-of', 'attack-pattern')
+def get_behavior_relationships(src, id):
+    return get_relationships_by(src, id, 'attack-pattern', 'subtechnique-of', 'attack-pattern')
 
-def get_behaviors_used_by_malware(id):
-    rels =  get_relationships_by(id, 'malware', 'uses', 'attack-pattern')
+def get_behaviors_used_by_malware(src, id):
+    rels =  get_relationships_by(src, id, 'malware', 'uses', 'attack-pattern')
 
     l = []
     for rel in rels:
-        l.append(get_behavior_by_id(rel.target_ref))
+        l.append(get_behavior_by_id(src, rel.target_ref))
 
     return l
 
-def get_malwares_using_behavior(id):
-    rels = get_relationships_by(id, 'malware', 'uses', 'attack-pattern', True)
+def get_malwares_using_behavior(src, id):
+    rels = get_relationships_by(src, id, 'malware', 'uses', 'attack-pattern', True)
 
     l = []
     for rel in rels:
-        l.append(get_malware_by_id(rel.source_ref))
+        l.append(get_malware_by_id(src, rel.source_ref))
         
     return l
 
 def setup_src():
-    global g_src
-    g_src = FileSystemSource('./mbc-stix2/')
+    return FileSystemSource('./mbc-stix2/')
 
 if __name__ == '__main__':
-    setup_src()
+    src = setup_src()
 
-    malware = get_malware_by_id('malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
-    malwares = get_behaviors_used_by_malware(malware.id)
+    malware = get_malware_by_id(src, 'malware--36e75009-8fd6-467a-aa8c-c6a4d3511dfa')
+    malwares = get_behaviors_used_by_malware(src ,malware.id)
     for m in malwares:
         print(str(m))
 
     print('=======')
-    behavior = get_behavior_by_external_id('B0031')
-    behaviors = get_malwares_using_behavior(behavior.id)
+    behavior = get_behavior_by_external_id(src, 'B0031')
+    behaviors = get_malwares_using_behavior(src, behavior.id)
     for b in behaviors:
         print(str(b))
