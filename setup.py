0ca0a36d0c28d23e1194f3f1638531549cdbf801
accidentalrebel <accidentalrebel@gmail.com>
2021-04-06 11:55:07 +0800

    Added readme.

--- setup.py
+++ setup.py
@@ -2,9 +2,15 @@
 
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='mbclib',
+    version='0.0.1',
     description='A library for querying the STIX data for the MBC (Malware Behavior Catalog).',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Karlo Licudine',
     author_email='karlolicudine@gmail.com',
     url='https://github.com/accidentalrebel/mbclib',
